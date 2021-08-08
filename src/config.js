6474ae82b55691dba3777a5c19634a50f2619dd3
Mikhail Konev <mkonev2020@gmail.com>
2021-08-08 18:27:50 +0600

    #1 api host settings applied

--- src/config.js
+++ src/config.js
@@ -1,5 +1,16 @@
+let port = 8000;
+let host = "http://localhost";
+
+if (process.env.REACT_APP_NODE_ENV === 'test') {
+  port = 8010;
+}
+
+if (process.env.REACT_APP_CI) {
+  host = "http://glamping-ci.ddns.net";
+}
+
 export default {
-  apiUrl: "http://localhost:8000",
+  apiUrl: host + ":" +port,
   artistsApiUrl: "http://localhost:8000/uploads/artists/",
   albumsApiUrl: "http://localhost:8000/uploads/albums/",
   tracksApiUrl: "http://localhost:8000/uploads/tracks/"
